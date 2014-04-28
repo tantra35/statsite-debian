cb6587d90564641d373c370ae26489d57f3b8b92
Daniil Guzanov <melkor217@gmail.com>
2014-04-28 09:49:29 +0400

    whoops.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(statsite):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="statsite" command="/usr/bin/statsite"
--- debian/statsite.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the statsite package
-#
-0 4	* * *	root	[ -x /usr/bin/statsite_maintenance ] && /usr/bin/statsite_maintenance
